9781557b137665cb096c3c5e78da565cf070bcc0
danny-wang <danny.wang@aliyun.com>
2021-07-06 11:15:01 +0800

    update

--- src/backend/distributed/executor/subplan_execution.c
+++ src/backend/distributed/executor/subplan_execution.c
@@ -64,7 +64,7 @@ void runSubPlanParallel(void *arg) {
 	ParamListInfo params = NULL;
 	char *resultId = GenerateResultId(planId, subPlanId);
 	if (IsLoggableLevel(DEBUG3)) {
-		ereport(DEBUG3, (errmsg("$$$$$$$$$$$$$$$$$$runSubPlanParallel resultId:%s, plannedStmt:%p" ,resultI,plannedStmt)));
+		ereport(DEBUG3, (errmsg("$$$$$$$$$$$$$$$$$$runSubPlanParallel resultId:%s, plannedStmt:%p" ,resultId,plannedStmt)));
 		//elog_node_display(LOG, "plannedStmt parse tree", plannedStmt, Debug_pretty_print);
 	}
 	ereport(DEBUG3, (errmsg("$$$$$$$$$$$$$$$$$$ STEP 1" )));
