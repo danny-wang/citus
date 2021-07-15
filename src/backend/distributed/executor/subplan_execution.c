40f183b39decbc9d49af4d2356e7404d324fab37
danny-wang <danny.wang@aliyun.com>
2021-07-15 12:55:05 +0800

    update

--- src/backend/distributed/executor/subplan_execution.c
+++ src/backend/distributed/executor/subplan_execution.c
@@ -551,8 +551,8 @@ OpenNewConnectionsV2(SubPlanParallel* subPlan) {
 	//ConnectionHashKey key;
 	bool found;
 	char *hostname = subPlan->nodeName;
-	if (strcmp(rte->eref->aliasname, "localhost") == 0) {
-		hostname = "172.31.87.38"
+	if (strcmp(subPlan->nodeName, "localhost") == 0) {
+		hostname = "172.31.87.38";
 	}
 	/* do some minimal input checks */
 	if (strlen(hostname) > MAX_NODE_LENGTH)
